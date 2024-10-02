f81a318ef867743a5a26b32d8ceae0e1d96953a1
johan guillen <sioupes34@gmail.com>
2024-10-02 15:02:48 +0200

    tp2

---
+++ approche-imperative/src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,24 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args){
+        byte b = 1;
+        short s= 2;
+        int i = 3;
+        long l =4;
+        float f = 0.5f;
+        double d = 0.55d;
+        char c = 'c';
+        boolean bo = true;
+        String st ="bonjour";
+        System.out.println(st) ;
+        String randomString = "Voici le résultat d'un calcul : 1+5=6";
+        System.out.println(randomString);
+        randomString= "Voici le résultat d'un calcul : \n 1+5=6";
+        System.out.println(randomString);
+    };
+
+
+
+
+}
