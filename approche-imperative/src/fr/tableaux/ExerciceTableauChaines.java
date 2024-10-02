1df81590ba06c5892f7de8938f508b8ebe7fee34
johan guillen <sioupes34@gmail.com>
2024-10-02 17:29:24 +0200

    tp4

--- approche-imperative/src/fr/tableaux/ExerciceTableauChaines.java
+++ approche-imperative/src/fr/tableaux/ExerciceTableauChaines.java
@@ -2,6 +2,11 @@ package fr.tableaux;
 
 public class ExerciceTableauChaines {
     public static void main(String[] args) {
-        
+        String[] ville = {"paris", "lyon", "montpellier", "marseille","nimes"};
+        System.out.println(ville[0]+" "+ville[1]+" "+ville[2]+" "+ville[3]+" "+ville[4]);
+        System.out.println(ville.length);
+        ville[3]="Reims";
+        System.out.println(ville[0]+" "+ville[1]+" "+ville[2]+" "+ville[3]+" "+ville[4]);
+
     }
 }
