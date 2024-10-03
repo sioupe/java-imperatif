737287acb3e8bbde05358a20ff9af97c0d491f19
johan guillen <sioupes34@gmail.com>
2024-10-03 10:51:32 +0200

    tp 5

---
+++ approche-imperative/src/fr/boucles/ExercicesBouclesBase.java
@@ -0,0 +1,18 @@
+package fr.boucles;
+
+public class ExercicesBouclesBase {
+    public static void main(String[] args) {
+        for (int i = 1; i <=10 ; i++) {
+            System.out.println(i);
+        }
+        for (int i = 0; i < 20; i++) {
+            System.out.println("guillen johan");
+        }
+        for (int i = 2; i <=100 ; i=i+2) {
+            System.out.println(i);
+        }
+        for (int i = 1; i <=99 ; i= i+2) {
+            System.out.println(i);
+        }
+    }
+}
