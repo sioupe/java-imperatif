fec3efa8b6873d5add2ce13d137561392a4e25da
johan guillen <sioupes34@gmail.com>
2024-10-03 12:08:50 +0200

    auf ex8

--- approche-imperative/src/fr/algorithmie/CalculMoyenne.java
+++ approche-imperative/src/fr/algorithmie/CalculMoyenne.java
@@ -3,6 +3,11 @@ package fr.algorithmie;
 public class CalculMoyenne {
     public static void main(String[] args) {
         int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
-
+        double moyenne =0.0;
+        for(int nb : array){
+            moyenne=moyenne + nb;
+        }
+        moyenne=moyenne / array.length;
+        System.out.println("moyenne = "+moyenne);
     }
 }
