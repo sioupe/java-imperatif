84272639fbddcf03b3e516edef4ca0057097a808
johan guillen <sioupes34@gmail.com>
2024-10-03 12:04:36 +0200

    auf ex7

--- approche-imperative/src/fr/algorithmie/RechercheMin.java
+++ approche-imperative/src/fr/algorithmie/RechercheMin.java
@@ -3,6 +3,13 @@ package fr.algorithmie;
 public class RechercheMin {
     public static void main(String[] args) {
         int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
+        int min =array[0];
+        for(int nb : array){
+            if (nb < min) {
+                min=nb;
+            }
 
+        }
+        System.out.println("nbMin = "+min);
     }
 }
