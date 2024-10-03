9915e15149daad0b6b4ef12c15a9e94189730682
johan guillen <sioupes34@gmail.com>
2024-10-03 11:14:15 +0200

    auf ex1

---
+++ approche-imperative/src/fr/algorithmie/AffichageIdentie.java
@@ -0,0 +1,9 @@
+package fr.algorithmie;
+
+public class AffichageIdentie {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Johan Guillen");
+        }
+    }
+}
